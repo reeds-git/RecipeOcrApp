58b4df78783bf310712559623b30f64fe446600a
reeds-git <atw06001@byui.edu>
2018-04-08 00:01:45 -0600

    Added scanning recipe activity and tesseract class to scan text from an image.

--- app/src/main/java/reed/com/recipeocrapp/MainActivity.java
+++ app/src/main/java/reed/com/recipeocrapp/MainActivity.java
@@ -1,13 +1,30 @@
 package reed.com.recipeocrapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Button scanRecipeActivityBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scanRecipeActivityBtn = (Button)findViewById(R.id.scanRecipeActivityBtn);
+
+        scanRecipeActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent scanRecipeActivityIntent = new Intent(getApplicationContext(), ScanRecipeActivity.class);
+                startActivity(scanRecipeActivityIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/reed/com/recipeocrapp/TessOCR.java
@@ -0,0 +1,44 @@
+package reed.com.recipeocrapp;
+
+import android.graphics.Bitmap;
+import android.os.Environment;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+
+/**
+ * Created by HP-laptop on 4/7/2018.
+ */
+
+public class TessOCR {
+
+    private TessBaseAPI mTess;
+
+    public TessOCR() {
+        // TODO Auto-generated constructor stub
+
+        mTess = new TessBaseAPI();
+        // AssetManager assetManager=
+        String datapath = Environment.getExternalStorageDirectory() + "/DemoOCR/";
+        String language = "eng";
+        // AssetManager assetManager = getAssets();
+        File dir = new File(datapath + "/tessdata/");
+        if (!dir.exists())
+            dir.mkdirs();
+        mTess.init(datapath, language);
+    }
+
+    public String getOCRResult(Bitmap bitmap) {
+
+        mTess.setImage(bitmap);
+        String result = mTess.getUTF8Text();
+
+        return result;
+    }
+
+    public void onDestroy() {
+        if (mTess != null)
+            mTess.end();
+    }
+}
